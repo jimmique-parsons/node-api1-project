5bc7d65e1bf920278c1322278a61cc4705f913c3
Jimmique <jimmique88@gmail.com>
2020-01-15 21:33:21 -0800

    Added GET request

--- index.js
+++ index.js
@@ -10,3 +10,39 @@ server.listen(3000, () => {
 });
 
 server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('Hi I can respond!');
+});
+
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: "The users information could not be retrieved."
+            });
+        });
+});
+
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+
+    db.findById(id)
+        .then(user => {
+            if (user) {
+                res.status(200).json(user);
+            } else {
+                res.status(404).json({
+                    error: "The user with the specified ID does not exist."
+                });
+            }
+        })
+        .catch(err => {
+            res.status(400).json({
+                error: "The user information could not be retrieved."
+            });
+        });
+});
