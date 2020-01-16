6b9c326e6b377ef895b311be32fd8f9baffe9da9
Jimmique <jimmique88@gmail.com>
2020-01-15 21:53:54 -0800

    Added DELETE handler

--- index.js
+++ index.js
@@ -67,3 +67,24 @@ server.get('/api/users/:id', (req, res) => {
             });
         });
 });
+
+server.delete('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+
+    db.findById(id)
+        .then(user => {
+            if (user) {
+                db.remove(id)
+                    .then(count => {
+                        if (count === 0) {
+                            res.status(500).json({ error: 'The user could not be removed' });
+                        } else {
+                            res.status(204).end();
+                        }
+                    })
+            } else {
+                res.status(404).json({ message: "The user with the specified ID does not exist." });
+            }
+        })
+
+});
